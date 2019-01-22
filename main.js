3a25b6ea8ee116830ed407b1909bcc0f51fa55fc
Gløer Olav Langslet <gloer@langslet.com>
2019-01-22 21:04:07 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+// HTML-elementer:
+const secMeldinger = document.querySelector("#secMeldinger");
+const skjema = document.querySelector("#skjema");
+const inpMelding = document.querySelector("#inpMelding");
+
+const db = firebase.database();
+const meldinger = db.ref("minemessengermeldinger");
+
+
+// Definerer en bruker
+let user;
+
+// Sjekker om vi er logget inn
+firebase.auth().onAuthStateChanged( newuser => {
+    if (newuser) {
+        // Setter user til den innloggede brukeren
+        user = newuser;
+    } else {
+        document.body.innerHTML = `
+            <main class="notloggedin>
+                <h1>Du er ikke logget inn</h1>
+                <a href="login.html">Logg inn her</a>
+            </main>            
+        `;
+    }
+});
+
+
+
+
+// Event Listeners
+// skjema.addEventListener("submit", lagreMelding);
+// meldinger.on("child_added", visMelding);
+
